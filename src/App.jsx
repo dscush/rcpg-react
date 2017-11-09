96eed46ca97d765b04cebf4d4ec6201dcbc5804a
Dan Cusher <dscush@gmail.com>
2017-11-09 06:00:59 +0000

    Adjust media query col widths and font sizes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ class App extends Component {
         </header>
         <div className="container">
           <div className="row">
-            <div className="col-xs-12">
+            <div className="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3">
               <PanelAddChord />
             </div>
           </div>
