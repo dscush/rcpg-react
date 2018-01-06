40ac90d68c7e88c8eb02c608480712d5bd1b9fa4
thepureinx <laszlo.develop@gmail.com>
2018-01-05 23:25:49 -0800

    fix pushing to generators

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import ChordEditor from './components/ChordEditor';
 import GeneratorManager from './components/GeneratorManager';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    let generators = [];
+  constructor() {
+    super();
     this.state = {
-      generators: generators,
-      currentPanel: <GeneratorManager
-        createGenerator={this.createGenerator}
-        selectGenerator={this.selectGenerator}
-        generators={generators.map((generator) => generator.name)}
-      />
+      generators: [],
     };
-
-    this.createGenerator = this.createGenerator.bind(this);
-    this.selectGenerator = this.selectGenerator.bind(this);
   }
 
   createGenerator(name) {
-    this.setState(function(state) {
-      state.generators.push({name: name, chords: []});
-      return {generators: state.generators};
-    });
+    this.setState(prevState => ({
+        generators: [...prevState.generators, {name: name, chord: []}]
+    }));
   }
 
   selectGenerator(name) {
     // TODO: create PanelEditGenerator and load it with the selected generator
     console.log(name);
+  }
+  
+  renderCurrentPanel = () => {
+    return (
+      <GeneratorManager
+        createGenerator={this.createGenerator.bind(this)}
+        selectGenerator={this.selectGenerator.bind(this)}
+        generators={this.state.generators.map((generator) => generator.name)}
+      />
+    );
   }
 
   render() {
@@ -41,7 +41,7 @@ class App extends Component {
         <div className="container">
           <div className="row">
             <div className="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3">
-              {this.state.currentPanel}
+              {this.renderCurrentPanel()}
             </div>
           </div>
         </div>
