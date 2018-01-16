7cd8d681d315a315daf9c3b81efc56e19be39293
Dan Cusher <dscush@gmail.com>
2018-01-15 22:12:13 -0500

    Move panel from GeneratorManager to GeneratorSelector

--- src/components/GeneratorManager.jsx
+++ src/components/GeneratorManager.jsx
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import Panel from './Panel';
 import GeneratorSelector from './GeneratorSelector';
 
 class GeneratorManager extends Component {
   render() {
     return (
-    <Panel title="Select or Create a Generator">
       <GeneratorSelector
         createGenerator={this.props.createGenerator}
         selectGenerator={this.props.selectGenerator}
         generators={this.props.generators}
       />
-    </Panel>
   )}
 }
 
--- src/components/GeneratorSelector.jsx
+++ src/components/GeneratorSelector.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Panel from './Panel';
 import CreationForm from './CreationForm';
 import SelectionForm from './SelectionForm';
 
@@ -10,31 +11,33 @@ class GeneratorSelector extends Component {
 
   render() {
     return (
-      <div>
+      <Panel>
         <div>
-          <h3>Create a Generator</h3>
-          <CreationForm
-            create={this.props.createGenerator}
-            id='generator-create'
-            existingNames={this.props.generators.map((generator) => generator.name)}
-          />
-        </div>
-        {this.props.generators.length !== 0 &&
           <div>
-            <h3>Select a Generator</h3>
-            <SelectionForm
-              options={this.props.generators.map((generator) => ({
-                key: generator.name,
-                value: generator.name,
-                label: generator.name
-              }))}
-              select={this.props.selectGenerator}
-              label='Generators'
-              id='generator-select'
+            <h3>Create a Generator</h3>
+            <CreationForm
+              create={this.props.createGenerator}
+              id='generator-create'
+              existingNames={this.props.generators.map((generator) => generator.name)}
             />
           </div>
-        }
-      </div>
+          {this.props.generators.length !== 0 &&
+            <div>
+              <h3>Select a Generator</h3>
+              <SelectionForm
+                options={this.props.generators.map((generator) => ({
+                  key: generator.name,
+                  value: generator.name,
+                  label: generator.name
+                }))}
+                select={this.props.selectGenerator}
+                label='Generators'
+                id='generator-select'
+              />
+            </div>
+          }
+        </div>
+      </Panel>
     );
   }
 }
